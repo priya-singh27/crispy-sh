aa04454dc32ac6c07df5fa81f592c6addcfeaa7f
priya-singh27 <priya.beingdev@gmail.com>
2025-01-18 17:45:26 +0530

    type builtin: builtins

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,7 +27,6 @@ public class Main {
              else {
                 System.out.println(input + ": command not found");
             }
-            System.out.print("$ ");
             input = sc.nextLine();
         }
 
