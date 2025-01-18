089cf85916cc103486565a7208bc534ed1858fe8
priya-singh27 <priya.beingdev@gmail.com>
2025-01-18 17:15:28 +0530

    echo builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,9 +8,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         do {
-            if (input.equals("exit 0")) break;
-            else if(input.matches("^\\s*\\$\\s*echo.*")) System.out.println(input);
-            else System.out.println(input + ": command not found");
+            if (input.equals("exit 0"))
+                break;
+                
+            System.out.println(input + ": command not found");
+            
+            if (input.startsWith("echo")) {
+                System.out.println(input.substring(5));
+            } else {
+                System.out.println(input + ": command not found");
+            }
             System.out.print("$ ");
             input = sc.nextLine();
         } while (!input.matches(""));
