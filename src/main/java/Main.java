c9dec3db9ff4bce93429e8726f2c37590d68e01f
priya-singh27 <priya.beingdev@gmail.com>
2025-01-18 15:51:02 +0530

    All commands are invalid implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,8 @@ public class Main {
         // Uncomment this block to pass the first stage
         System.out.print("$ ");
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(input+ ": command not found");
     }
 }
