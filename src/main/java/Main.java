fe83dd41acd4cb21783721728252c9ea6c9f33b2
priya-singh27 <priya.beingdev@gmail.com>
2025-01-18 18:12:56 +0530

    type builtin: builtins

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-        System.out.print("$ ");
 
         Scanner sc = new Scanner(System.in);
         String input, typeSubstring;
